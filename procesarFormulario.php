b8715eabc8cf0600f22ea0effea2041aac03fe7a
Federico Manuel Peche <136661406+FedeMPeche@users.noreply.github.com>
2023-11-01 21:48:11 -0300

    Update procesarFormulario.php

--- procesarFormulario.php
+++ procesarFormulario.php
@@ -1,32 +1,46 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["nombre"];
+    $nombre = $_POST["nombre"];
     $email = $_POST["email"];
-    $subject = $_POST["telefono"];
-    $message = $_POST["mensaje"];
-    error_log(print_r($_POST, true));
+    $telefono = $_POST["telefono"];
+    $mensaje = $_POST["mensaje"];
 
-    // Validación de reCAPTCHA
-    $secretAPIkey = '6Lc43eUoAAAAAEth8I45VAjLymQtAfzoZT0fCJRc';
-    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretAPIkey . '&response=' . $_POST['g-recaptcha-response']);
-    $response = json_decode($verifyResponse);
+    // Clave secreta de reCAPTCHA v3
+    $clave_secreta = "6LdjeOYoAAAAAE6Sv63qqL-NwNDgFe8DrvVgtned"; // Reemplaza con tu clave secreta
 
-    if ($response->success) {
-        $to = "yubesrock@gmail.com";
+    // Verificación de reCAPTCHA
+    $recaptcha_response = $_POST["g-recaptcha-response"];
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = [
+        'secret' => $clave_secreta,
+        'response' => $recaptcha_response
+    ];
+    $options = [
+        'http' => [
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        ]
+    ];
+    $context = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+    $recaptcha_data = json_decode($response);
 
-        $response = array(
-            "status" => "success",
-            "message" => "¡Muchas gracias! Pronto nos pondremos en contacto. También puedes visitar nuestras redes sociales para recibir una respuesta rápida. ¡Saludos!"
-        );
+    if ($recaptcha_data->success) {
+        // Envía el correo
+        $destinatario = "yubesrock@yubes.com.ar"; // Reemplaza con tu dirección de correo
+        $asunto = "Nuevo mensaje de contacto de $nombre";
+        $contenido = "Nombre: $nombre\nEmail: $email\nTeléfono: $telefono\nMensaje: $mensaje";
+
+        // Usamos la función mail() para enviar el correo
+        $enviado = mail($destinatario, $asunto, $contenido);
+
+        if ($enviado) {
+            echo "¡Mensaje enviado con éxito!";
+        } else {
+            echo "Error al enviar el mensaje. Por favor, inténtalo de nuevo.";
+        }
     } else {
-        $response = array(
-            "status" => "error",
-            "message" => "Verificación de reCAPTCHA fallida. Inténtalo de nuevo."
-        );
+        echo "Error de verificación de reCAPTCHA. Por favor, intenta de nuevo.";
     }
-
-    // Devuelve la respuesta como JSON
-    header('Content-Type: application/json');
-    echo json_encode($response);
 }
 ?>
